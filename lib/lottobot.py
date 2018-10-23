292e7d4a0600f5c08fa21b02cdeb507fd70f5856
Erik Letson <hmagellan@tutamail.com>
2018-10-23 18:33:18 -0500

    Basic start

--- lib/lottobot.py
+++ lib/lottobot.py
@@ -1,3 +1,18 @@
 import steem
 
+class Lottobot():
 
+    """
+    Class for the Lottobot main object.
+    """
+
+    def __init__(self, node, account):
+
+        self.node = node
+        self.account = account
+
+        self.steem = steem.Steem()
+
+    def mainloop(self):
+
+        pass
---
+++ start.py
@@ -0,0 +1,11 @@
+from lib import lottobot
+
+def Main():
+
+    l = lottobot.Lottobot(None, 'lottobot')
+
+    l.mainloop()
+
+if __name__ == '__main__':
+
+    Main()
